d3a9a843ccfb41c47c0121e8d139346151d88682
GEDIK marc <gedik@informatique.univ-paris-diderot.fr>
2014-11-14 01:40:56 +0100

    Record and RecordField checker

--- src/datix/datixTypechecker.ml
+++ src/datix/datixTypechecker.ml
@@ -25,7 +25,7 @@ module TypingEnvironment : sig
   (** [bind env x ty] returns an environment that assigns [ty]
       the variable [x] and extends [env]. *)
   val bind
-    : t -> identifier -> typ -> t
+      : t -> identifier -> typ -> t
 
   (** [UnboundIdentifier x] is raised if [x] is unbound. *)
   exception UnboundIdentifier of identifier
@@ -33,16 +33,16 @@ module TypingEnvironment : sig
   (** [lookup env x] returns the type assigned to [x] in [env].
       Raise {!UnboundIdentifier x} if no such variable exists. *)
   val lookup
-    : t -> identifier -> typ
+      : t -> identifier -> typ
 
   (** [bind_function env f s] assigns the signature [s] to [f]
       in [env] and extends [env]. *)
   val bind_function
-    : t -> function_identifier -> signature -> t
+      : t -> function_identifier -> signature -> t
 
   (** [defined_function env f] returns [true] iff [f] is bound in [env]. *)
   val defined_function
-    : t -> function_identifier -> bool
+      : t -> function_identifier -> bool
 
   (** [UnboundFunctionIdentifier f] is raised if [f] is unbound. *)
   exception UnboundFunctionIdentifier of function_identifier
@@ -51,12 +51,12 @@ module TypingEnvironment : sig
       in [env]. Raise {!UnboundFunctionIdentifier f] if no such
       [f] exists. *)
   val lookup_function
-    : t -> function_identifier -> signature
+      : t -> function_identifier -> signature
 
   (** [bind_type_definition env t tdef] introduces a new type definition
       in [env]. *)
   val bind_type_definition
-    : t -> type_identifier -> type_definition -> t
+      : t -> type_identifier -> type_definition -> t
 
   (** [UnboundTypeIdentifier t] is raised if no such type [t] exists. *)
   exception UnboundTypeIdentifier of type_identifier
@@ -68,12 +68,12 @@ module TypingEnvironment : sig
       of the record type that contains [l] as well as its definition.
       May raise {!UnboundLabel l}. *)
   val lookup_recordtype_from_label
-    : t -> label -> type_identifier * (label * typ) list
+      : t -> label -> type_identifier * (label * typ) list
 
   (** [lookup_recordtype env t] returns all the field types of
       the record type [t] in [env]. May raise {!UnboundTypeIdentifier l}.*)
   val lookup_recordtype
-    : t -> type_identifier -> (label * typ) list
+      : t -> type_identifier -> (label * typ) list
 
   (** [UnboundTag t] is raised if no such tag [t] exists. *)
   exception UnboundTag of tag
@@ -83,7 +83,10 @@ module TypingEnvironment : sig
       as all the types of the tag parameters.
       May raise {!UnboundTag t}. *)
   val lookup_tagged_union_type_from_tag
-    : t -> tag -> type_identifier * (tag * typ list) list
+      : t -> tag -> type_identifier * (tag * typ list) list
+
+  (** [find_record_type tenv l] returns the typ of [l] in [env] *)
+  val find_record_type : t -> (label * typ) list -> typ
 
 end = struct
   type t = {
@@ -99,8 +102,8 @@ end = struct
   }
 
   let bind e x ty = { e with
-    variables = (x, ty) :: e.variables
-  }
+		      variables = (x, ty) :: e.variables
+		    }
 
   exception UnboundIdentifier of identifier
 
@@ -111,8 +114,8 @@ end = struct
       raise (UnboundIdentifier x)
 
   let bind_function e f s = { e with
-    functions = (f, s) :: e.functions
-  }
+			      functions = (f, s) :: e.functions
+			    }
 
   let defined_function e f =
     List.mem_assoc f e.functions
@@ -139,21 +142,21 @@ end = struct
 
   let lookup_recordtype env t =
     match lookup_typedef env t with
-      | RecordTy fs -> fs
-      | _ -> raise (NotRecordType t)
+    | RecordTy fs -> fs
+    | _ -> raise (NotRecordType t)
 
   exception UnboundLabel of label
 
   let lookup_recordtype_from_label env l =
     try
       match List.find (fun (_, tdef) ->
-        match tdef with
-          | RecordTy fs -> List.exists (fun (l', _) -> l' = l) fs
-          | _ -> false
-      ) env.typedefs
+		       match tdef with
+		       | RecordTy fs -> List.exists (fun (l', _) -> l' = l) fs
+		       | _ -> false
+		      ) env.typedefs
       with
-        | (i, RecordTy fs) -> (i, fs)
-        | _ -> assert false (* Because of the predicate below. *)
+      | (i, RecordTy fs) -> (i, fs)
+      | _ -> assert false (* Because of the predicate below. *)
     with Not_found ->
       raise (UnboundLabel l)
 
@@ -162,15 +165,25 @@ end = struct
   let lookup_tagged_union_type_from_tag env t =
     try
       match List.find (fun (_, tdef) ->
-        match tdef with
-          | TaggedUnionTy ts -> List.exists (fun (t', _) -> t' = t) ts
-          | _ -> false
-      ) env.typedefs
+		       match tdef with
+		       | TaggedUnionTy ts -> List.exists (fun (t', _) -> t' = t) ts
+		       | _ -> false
+		      ) env.typedefs
       with
-        | (i, TaggedUnionTy ts) -> (i, ts)
-        | _ -> assert false (* Because of the predicate below. *)
+      | (i, TaggedUnionTy ts) -> (i, ts)
+      | _ -> assert false (* Because of the predicate below. *)
     with Not_found ->
       raise (UnboundTag t)
+
+  exception NotARecord
+
+  let find_record_type tenv fs =
+    let rec aux = function
+      | [] -> raise NotARecord
+      | (x,RecordTy y)::l when y = fs -> TyIdentifier x
+      | _::l -> aux l
+    in
+    aux tenv.typedefs
 
 end
 
@@ -181,17 +194,26 @@ let initial_typing_environment () =
   TypingEnvironment.empty
 (* TODO Ajouter les taggedUnion ?*)
 
+let rec check_record_label l fs =
+  match l,fs with
+  | [], [] -> ()
+  | (lb1,_)::l, (lb2,_)::fs ->
+     if lb1 = lb2
+     then check_record_label l fs
+     else failwith "Unbound record field lb2"
+  | [], (lb, _)::l -> failwith "Unbound record field lb"
+  | l, [] -> failwith "Some record fields are undefined"
+
 (** [typecheck tenv ast] checks that [ast] is a well-formed program
     under the typing environment [tenv]. *)
 let typecheck tenv ast =
   let rec program tenv p =
     List.fold_left definition tenv p
 
-
   and definition tenv def =
     match Position.value def with
     | DefineValue (p, e) ->
-      define_value tenv p e
+       define_value tenv p e
 
     | DefineFunction (f, xs, _, e) ->
        let typs = snd (List.split xs) in
@@ -202,7 +224,7 @@ let typecheck tenv ast =
        TypingEnvironment.bind_function tenv (Position.value f) signature
 
     | DefineType (t, tdef) ->
-      TypingEnvironment.bind_type_definition tenv t tdef
+       TypingEnvironment.bind_type_definition tenv t tdef
 
 
   (** [define_value tenv p e] returns a new environment that associates
@@ -215,60 +237,72 @@ let typecheck tenv ast =
       [e] under the environment [tenv] if [e] is well-typed. *)
   and infer_expression_type tenv e =
     match Position.value e with
-      | Literal l ->
-        infer_literal_type l
+    | Literal l ->
+       infer_literal_type l
 
-      | Variable x ->
-        (try
+    | Variable x ->
+       (try
            TypingEnvironment.lookup tenv x
          with TypingEnvironment.UnboundIdentifier (Id x) ->
            error
              (Position.position e)
              (Printf.sprintf "Identifier `%s' is unbound." x)
-        )
+       )
 
-      | Define (p, e1, e2) ->
-        let tenv = define_value tenv p e1 in
-        infer_expression_type tenv e2
+    | Define (p, e1, e2) ->
+       let tenv = define_value tenv p e1 in
+       infer_expression_type tenv e2
 
-      (* TODO funcall binop *)
-      | FunCall (FunId id as f, es) ->
-	 let Signature (l, typ) = TypingEnvironment.lookup_function tenv f in
-	 (* TODO tester type des arguments *)
-	 typ
+    (* TODO funcall binop *)
+    | FunCall (FunId id as f, es) ->
+       let Signature (l, typ) = TypingEnvironment.lookup_function tenv f in
+       (* TODO tester type des arguments *)
+       typ
 
-      | IfThenElse (c, te, fe) ->
-	 let c = infer_expression_type tenv c in
-	 if c  = tybool
-	 then
-	   begin
-	     let te = infer_expression_type tenv te in
-	     let fe = infer_expression_type tenv fe in
-	     if te = fe
-	     then te
-	     else failwith "Todo raise pas meme type"
-           end
-	 else
-	   failwith "Todo raise condition pas un bool"
+    | IfThenElse (c, te, fe) ->
+       let c = infer_expression_type tenv c in
+       if c  = tybool
+       then
+	 begin
+	   let te = infer_expression_type tenv te in
+	   let fe = infer_expression_type tenv fe in
+	   if te = fe
+	   then te
+	   else failwith "Todo raise pas meme type"
+         end
+       else
+	 failwith "Todo raise condition pas un bool"
 
-      | Tuple es ->
-	 let typs = List.map (infer_expression_type tenv) es in
-	 TyTuple typs
+    | Tuple es ->
+       let typs = List.map (infer_expression_type tenv) es in
+       TyTuple typs
 
-      | Record [] ->
-        assert false (* By parsing. *)
+    | Record [] ->
+       assert false (* By parsing. *)
 
-      | Record fs ->
-	 failwith "13Student! This is your job!"
+    | Record fs ->
+       let t,l = TypingEnvironment.lookup_recordtype_from_label tenv (fst (List.hd fs))
+       in
+       let fs = List.sort compare fs in
+       let l  = List.sort compare l  in
+       check_record_label l fs;
+       TyIdentifier t
 
-      | RecordField (e, (Label lid as l)) ->
-         failwith "14Student! This is your job!"
+    | RecordField (e, (Label lid as l)) ->
+       let id =
+	 match infer_expression_type tenv e with
+	 | TyIdentifier id -> id
+	 | _ -> failwith "L'expression n'est pas un record"
+       in
+       let typs = TypingEnvironment.lookup_recordtype tenv id in
+       List.assoc l typs
 
-      | TaggedValues (k, es) ->
-         failwith "15Student! This is your job!"
+    | TaggedValues (k, es) ->
+       failwith "15Student! This is your job!"
 
-      | Case (e, bs) ->
-        failwith "16Student! This is your job!"
+    | Case (e, bs) ->
+       let etyp = infer_expression_type tenv e in
+       infer_branches tenv etyp None bs
 
   (** [infer_branches tenv pty previous_branch_type (Branch (p, e))]
       checks that the pattern [p] has type [pty] and that the type of
@@ -276,36 +310,44 @@ let typecheck tenv ast =
       branch (unless this is the first branch). *)
   and infer_branches tenv pty previous_branch_type = function
     | [] ->
-      begin match previous_branch_type with
-        | None -> assert false (* By parsing. *)
-        | Some ty -> ty
-      end
+       begin match previous_branch_type with
+             | None -> assert false (* By parsing. *)
+             | Some ty -> ty
+       end
     | Branch (pat, e) :: bs ->
-       failwith "17Student! This is your job!"
+       let tenv = check_pattern tenv pty pat in
+       let etyp = infer_expression_type tenv e in
+       begin
+	 match previous_branch_type with
+         | None -> infer_branches tenv pty (Some etyp) bs
+         | Some ty -> if etyp = ty
+		      then infer_branches tenv pty (Some ty) bs
+		      else failwith("pas le meme type")
+       end
 
   (** [check_pattern tenv pty pat] checks that [pat] can be assigned
       the type [pty] and, if so, returns an extension of [tenv] with
       the variables of [pat] bound to their respective types. *)
   and check_pattern tenv pty pat =
     match Position.value pat, pty with
-      | PVariable x, _ ->
-	 check_variable tenv pty x
+    | PVariable x, _ ->
+       check_variable tenv pty x
 
-      | PTuple xs, TyTuple tys ->
-	 check_same_length (Position.position pat) xs tys;
-	 List.fold_left2 check_variable tenv tys xs
+    | PTuple xs, TyTuple tys ->
+       check_same_length (Position.position pat) xs tys;
+       List.fold_left2 check_variable tenv tys xs
 
-      | PWildcard, _ ->
-	 tenv
+    | PWildcard, _ ->
+       tenv
 
-      | PTaggedValues (k, xs), TyIdentifier t ->
-         failwith "21Student! This is your job!"
+    | PTaggedValues (k, xs), TyIdentifier t ->
+       failwith "Student..."
 
-      | _, _ ->
-        error (Position.position pat) (
-          Printf.sprintf "This pattern has not type: %s\n"
-            (DatixPrettyPrinter.(to_string typ pty))
-        )
+    | _, _ ->
+       error (Position.position pat) (
+               Printf.sprintf "This pattern has not type: %s\n"
+			      (DatixPrettyPrinter.(to_string typ pty))
+             )
 
   and check_variable tenv ty x =
     TypingEnvironment.bind tenv x ty
@@ -314,24 +356,26 @@ let typecheck tenv ast =
     let ity = infer_expression_type tenv e in
     if ity <> xty then
       error (Position.position e) (
-        Printf.sprintf "Incompatible types.\nExpected: %s\nInferred: %s\n"
-          (DatixPrettyPrinter.(to_string typ xty))
-          (DatixPrettyPrinter.(to_string typ ity))
-      )
+              Printf.sprintf "Incompatible types.\nExpected: %s\nInferred: %s\n"
+			     (DatixPrettyPrinter.(to_string typ xty))
+			     (DatixPrettyPrinter.(to_string typ ity))
+	    )
 
-  and check_same_length : type a b. Position.t -> a list -> b list -> unit = fun pos a b ->
+  and check_same_length :
+type a b. Position.t -> a list -> b list -> unit =
+    fun pos a b ->
     let aln = List.length a and bln = List.length b in
     if (aln <> bln) then (
       error pos
-        (Printf.sprintf
-           "Invalid number of arguments.\nExpected: %d\nGiven: %d\n."
-           aln bln
-        )
+	    (Printf.sprintf
+	       "Invalid number of arguments.\nExpected: %d\nGiven: %d\n."
+	       aln bln
+	    )
     )
 
-  and infer_literal_type = function
-    | LInt _ ->
+ and infer_literal_type = function
+   | LInt _ ->
       tyint
 
-  in
-  program tenv ast
+     in
+     program tenv ast
