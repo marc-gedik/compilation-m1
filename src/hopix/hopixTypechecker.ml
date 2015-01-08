fccab8660126ff11f9456ed193bbac28fcd3c27f
GEDIK marc <gedik@informatique.univ-paris-diderot.fr>
2015-01-08 13:00:06 +0100

    Typechecker fun, recfuns, apply

--- src/hopix/hopixTypechecker.ml
+++ src/hopix/hopixTypechecker.ml
@@ -21,7 +21,7 @@ module TypingEnvironment : sig
   (** [bind env x ty] returns an environment that assigns [ty]
       the variable [x] and extends [env]. *)
   val bind
-    : t -> identifier -> typ -> t
+      : t -> identifier -> typ -> t
 
   (** [UnboundIdentifier x] is raised if [x] is unbound. *)
   exception UnboundIdentifier of identifier
@@ -29,12 +29,12 @@ module TypingEnvironment : sig
   (** [lookup env x] returns the type assigned to [x] in [env].
       Raise {!UnboundIdentifier x} if no such variable exists. *)
   val lookup
-    : t -> identifier -> typ
+      : t -> identifier -> typ
 
   (** [bind_type_definition env t tdef] introduces a new type definition
       in [env]. *)
   val bind_type_definition
-    : t -> type_identifier -> type_definition -> t
+      : t -> type_identifier -> type_definition -> t
 
   (** [UnboundTypeIdentifier t] is raised if no such type [t] exists. *)
   exception UnboundTypeIdentifier of type_identifier
@@ -43,7 +43,7 @@ module TypingEnvironment : sig
       [env]. May raise {!UnboundTypeIdentifier t} if no such definition
       exists. *)
   val lookup_type_definition
-    : t -> type_identifier -> type_definition
+      : t -> type_identifier -> type_definition
 
   (** [UnboundLabel l] is raised if no record type contains [l] as a label. *)
   exception UnboundLabel of label
@@ -52,7 +52,7 @@ module TypingEnvironment : sig
       of the record type that contains [l] as well as its definition.
       May raise {!UnboundLabel l}. *)
   val lookup_recordtype_from_label
-    : t -> label -> type_identifier * (label * typ) list
+      : t -> label -> type_identifier * (label * typ) list
 
   (** This exception is raised when a type identifier is defined but
       it is not a record type. *)
@@ -62,7 +62,7 @@ module TypingEnvironment : sig
       the record type [t] in [env].
       May raise {!UnboundTypeIdentifier l} or {!NotRecordType t}.*)
   val lookup_recordtype
-    : t -> type_identifier -> (label * typ) list
+      : t -> type_identifier -> (label * typ) list
 
   (** [UnboundTag t] is raised if no such tag [t] exists. *)
   exception UnboundTag of tag
@@ -72,7 +72,7 @@ module TypingEnvironment : sig
       as all the types of the tag parameters.
       May raise {!UnboundTag t}. *)
   val lookup_tagged_union_type_from_tag
-    : t -> tag -> type_identifier * (tag * typ list) list
+      : t -> tag -> type_identifier * (tag * typ list) list
 
   (** This exception is raised if a type identifier is defined but it
       is not a tagged union. *)
@@ -82,7 +82,7 @@ module TypingEnvironment : sig
       the tagged union type [t] in [env].
       May raise {!UnboundTypeIdentifier t} or {!NotTaggedUnion t}. *)
   val lookup_tagged_union_type
-    : t -> type_identifier -> (tag * typ list) list
+      : t -> type_identifier -> (tag * typ list) list
 
 end = struct
   type t = {
@@ -96,8 +96,8 @@ end = struct
   }
 
   let bind e x ty = { e with
-    variables = (x, ty) :: e.variables
-  }
+		      variables = (x, ty) :: e.variables
+		    }
 
   exception UnboundIdentifier of identifier
 
@@ -121,21 +121,21 @@ end = struct
 
   let lookup_recordtype env t =
     match lookup_type_definition env t with
-      | RecordTy fs -> fs
-      | _ -> raise (NotRecordType t)
+    | RecordTy fs -> fs
+    | _ -> raise (NotRecordType t)
 
   exception UnboundLabel of label
 
   let lookup_recordtype_from_label env l =
     try
       match List.find (fun (_, tdef) ->
-        match tdef with
-          | RecordTy fs -> List.exists (fun (l', _) -> l' = l) fs
-          | _ -> false
-      ) env.typedefs
+		       match tdef with
+		       | RecordTy fs -> List.exists (fun (l', _) -> l' = l) fs
+		       | _ -> false
+		      ) env.typedefs
       with
-        | (i, RecordTy fs) -> (i, fs)
-        | _ -> assert false (* Because of the predicate below. *)
+      | (i, RecordTy fs) -> (i, fs)
+      | _ -> assert false (* Because of the predicate below. *)
     with Not_found ->
       raise (UnboundLabel l)
 
@@ -144,13 +144,13 @@ end = struct
   let lookup_tagged_union_type_from_tag env t =
     try
       match List.find (fun (_, tdef) ->
-        match tdef with
-          | TaggedUnionTy ts -> List.exists (fun (t', _) -> t' = t) ts
-          | _ -> false
-      ) env.typedefs
+		       match tdef with
+		       | TaggedUnionTy ts -> List.exists (fun (t', _) -> t' = t) ts
+		       | _ -> false
+		      ) env.typedefs
       with
-        | (i, TaggedUnionTy ts) -> (i, ts)
-        | _ -> assert false (* Because of the predicate below. *)
+      | (i, TaggedUnionTy ts) -> (i, ts)
+      | _ -> assert false (* Because of the predicate below. *)
     with Not_found ->
       raise (UnboundTag t)
 
@@ -158,8 +158,8 @@ end = struct
 
   let lookup_tagged_union_type env t =
     match lookup_type_definition env t with
-      | TaggedUnionTy ks -> ks
-      | _ -> raise (NotTaggedUnion t)
+    | TaggedUnionTy ks -> ks
+    | _ -> raise (NotTaggedUnion t)
 
 end
 
@@ -180,7 +180,7 @@ let initial_typing_environment () =
   tenv
 
 let is_bool c =
-   c = tybool || c = (TyIdentifier (TId "bool"))
+  c = tybool || c = (TyIdentifier (TId "bool"))
 
 let check_record_label pos l fs =
   let rec aux l fs =
@@ -195,6 +195,11 @@ let check_record_label pos l fs =
   in
   aux l fs
 
+let lambda pos x =
+  match x with
+  | id, Some typ -> id, typ
+  | _, None -> error pos "lambda with no type"
+
 (** [typecheck tenv ast] checks that [ast] is a well-formed program
     under the typing environment [tenv]. *)
 let typecheck tenv ast =
@@ -204,12 +209,12 @@ let typecheck tenv ast =
 
   and definition tenv def =
     match Position.value def with
-      | DefineValue (p, e) ->
-        define_value tenv p e
+    | DefineValue (p, e) ->
+       define_value tenv p e
 
-      | DefineType (t, tdef) ->
-        well_formed_type_definition (Position.position def) tenv tdef;
-        TypingEnvironment.bind_type_definition tenv t tdef
+    | DefineType (t, tdef) ->
+       well_formed_type_definition (Position.position def) tenv tdef;
+       TypingEnvironment.bind_type_definition tenv t tdef
 
   and well_formed_type_definition pos tenv = function
     | RecordTy ltys ->
@@ -232,84 +237,97 @@ let typecheck tenv ast =
   and infer_expression_type tenv e =
     let pos = Position.position e in
     match Position.value e with
-      | Fun (x, e) ->
-           failwith "Student! This is your job!4"
+    | MutateTuple _ ->
+       failwith "MutataTuple typechecker"
 
-      | RecFuns fs ->
-           failwith "Student! This is your job!5"
+    | Fun (x, e) ->
+       let id, typ = lambda pos x in
+       let eTyp = infer_expression_type tenv e in
+       TyArrow (eTyp, typ)
 
-      | Apply (a, b) ->
-           failwith "Student! This is your job!6"
+    | RecFuns fs ->
+       let typ =
+	 List.map (fun (x, _) -> lambda (Position.position x) (Position.value x)) fs in
+       let eTyp =
+	 List.map (fun (_, e) -> infer_expression_type tenv e) fs in
+       let tyArrows =
+	 List.map2 (fun eTyp (_,typ) -> TyArrow(eTyp, typ)) eTyp typ in
+       TyTuple(tyArrows)
 
-      | Literal l ->
-         infer_literal_type l
+    | Apply (a, b) ->
+       let ta = infer_expression_type tenv a in
+       (match ta with
+	| TyArrow (typ, ret) -> check_expression_type tenv typ b; ret
+	| _ -> error pos "Not applicable"
+       )
 
-      | Variable x ->
-         (try
-             TypingEnvironment.lookup tenv x
-           with TypingEnvironment.UnboundIdentifier (Id x) ->
-             error
-               (Position.position e)
-               (Printf.sprintf "Identifier `%s' is unbound." x)
-	 )
+    | Literal l ->
+       infer_literal_type l
 
-      | Define (p, e1, e2) ->
-         let tenv = define_value tenv p e1 in
-	 infer_expression_type tenv e2
+    | Variable x ->
+       (try
+           TypingEnvironment.lookup tenv x
+         with TypingEnvironment.UnboundIdentifier (Id x) ->
+           error pos (Printf.sprintf "Identifier `%s' is unbound." x)
+       )
 
-      | IfThenElse (c, te, fe) ->
-         let c = infer_expression_type tenv c in
-	 let b = is_bool c in
-	 if b
-	 then
-	   begin
-	     let te = infer_expression_type tenv te in
-	     let fe = infer_expression_type tenv fe in
-	     if te = fe
-	     then te
-	     else error pos "Not same type"
-           end
-	 else
-	   error pos "Not a boolean"
+    | Define (p, e1, e2) ->
+       let tenv = define_value tenv p e1 in
+       infer_expression_type tenv e2
 
-      | Tuple es ->
-         let typs = List.map (infer_expression_type tenv) es in
-	 TyTuple typs
+    | IfThenElse (c, te, fe) ->
+       let c = infer_expression_type tenv c in
+       let b = is_bool c in
+       if b
+       then
+	 begin
+	   let te = infer_expression_type tenv te in
+	   let fe = infer_expression_type tenv fe in
+	   if te = fe
+	   then te
+	   else error pos "Not same type"
+         end
+       else
+	 error pos "Not a boolean"
 
-      | Record [] ->
-        assert false (* By parsing. *)
+    | Tuple es ->
+       let typs = List.map (infer_expression_type tenv) es in
+       TyTuple typs
 
-      | Record fs ->
-         let t,l = TypingEnvironment.lookup_recordtype_from_label tenv (fst (List.hd fs))
-	 in
-	 let fs = List.sort compare fs in
-	 let l  = List.sort compare l  in
-	 check_record_label pos l fs;
-	 TyIdentifier t
+    | Record [] ->
+       assert false (* By parsing. *)
 
-      | RecordField (e, (Label lid as l)) ->
-         let id =
-	   match infer_expression_type tenv e with
-	   | TyIdentifier id -> id
-	   | _ -> error pos "Not a record"
-	 in
-	 let typs = TypingEnvironment.lookup_recordtype tenv id in
-	 List.assoc l typs
+    | Record fs ->
+       let t,l = TypingEnvironment.lookup_recordtype_from_label tenv (fst (List.hd fs))
+       in
+       let fs = List.sort compare fs in
+       let l  = List.sort compare l  in
+       check_record_label pos l fs;
+       TyIdentifier t
 
-      | TaggedValues (k, es) ->
-         let id, union = TypingEnvironment.lookup_tagged_union_type_from_tag tenv k in
-	 let typs = List.assoc k union in
-	 check_same_length pos es typs;
-	 List.iter2 (fun e typ -> if (infer_expression_type tenv e) <> typ
-				  then error pos "Error in args"
-				  else ())
-		    es
-		    typs;
-	 TyIdentifier id
+    | RecordField (e, (Label lid as l)) ->
+       let id =
+	 match infer_expression_type tenv e with
+	 | TyIdentifier id -> id
+	 | _ -> error pos "Not a record"
+       in
+       let typs = TypingEnvironment.lookup_recordtype tenv id in
+       List.assoc l typs
 
-      | Case (e, bs) ->
-         let etyp = infer_expression_type tenv e in
-	 infer_branches tenv etyp None bs
+    | TaggedValues (k, es) ->
+       let id, union = TypingEnvironment.lookup_tagged_union_type_from_tag tenv k in
+       let typs = List.assoc k union in
+       check_same_length pos es typs;
+       List.iter2 (fun e typ -> if (infer_expression_type tenv e) <> typ
+				then error pos "Error in args"
+				else ())
+		  es
+		  typs;
+       TyIdentifier id
+
+    | Case (e, bs) ->
+       let etyp = infer_expression_type tenv e in
+       infer_branches tenv etyp None bs
 
 
 
@@ -319,20 +337,20 @@ let typecheck tenv ast =
       branches [bs]. *)
   and check_exhaustiveness pos ks = function
     | [] ->
-         failwith "Student! This is your job!16"
+       failwith "Student! This is your job!16"
     | Branch (pat, _) :: bs ->
-         failwith "Student! This is your job!17"
+       failwith "Student! This is your job!17"
 
-      (** [infer_branches tenv pty previous_branch_type (Branch (p, e))]
+  (** [infer_branches tenv pty previous_branch_type (Branch (p, e))]
           checks that the pattern [p] has type [pty] and that the type of
           [e] (if it exists) is the same as the one of the previous
           branch (unless this is the first branch). *)
   and infer_branches tenv pty previous_branch_type = function
     | [] ->
-      begin match previous_branch_type with
-        | None -> assert false (* By parsing. *)
-        | Some ty -> ty
-      end
+       begin match previous_branch_type with
+             | None -> assert false (* By parsing. *)
+             | Some ty -> ty
+       end
     | Branch (pat, e) :: bs ->
        let tenv = check_pattern tenv pty pat in
        let etyp = infer_expression_type tenv e in
@@ -349,35 +367,35 @@ let typecheck tenv ast =
       the variables of [pat] bound to their respective types. *)
   and check_pattern tenv pty pat =
     match Position.value pat, pty with
-      | PVariable x, _ ->
-         check_variable tenv pty x
+    | PVariable x, _ ->
+       check_variable tenv pty x
 
-      | PTuple xs, TyTuple tys ->
-         check_same_length (Position.position pat) xs tys;
-	 List.fold_left2 check_variable tenv tys xs
+    | PTuple xs, TyTuple tys ->
+       check_same_length (Position.position pat) xs tys;
+       List.fold_left2 check_variable tenv tys xs
 
-      | PWildcard, _ ->
-         tenv
+    | PWildcard, _ ->
+       tenv
 
-      | PTaggedValues (k, xs), TyIdentifier t ->
-         let union = TypingEnvironment.lookup_tagged_union_type tenv t in
-	 let typs  = List.assoc k union in
-	 check_same_length (Position.position pat) xs typs;
-	 List.fold_left2 check_variable tenv typs xs
+    | PTaggedValues (k, xs), TyIdentifier t ->
+       let union = TypingEnvironment.lookup_tagged_union_type tenv t in
+       let typs  = List.assoc k union in
+       check_same_length (Position.position pat) xs typs;
+       List.fold_left2 check_variable tenv typs xs
 
-      | _, _ ->
-        error (Position.position pat) (
-          Printf.sprintf "This pattern has not type: %s\n"
-            (HopixPrettyPrinter.(to_string typ pty))
-        )
+    | _, _ ->
+       error (Position.position pat) (
+               Printf.sprintf "This pattern has not type: %s\n"
+			      (HopixPrettyPrinter.(to_string typ pty))
+             )
 
   and check_irrefutable_pattern tenv pat : unit =
     match (Position.value pat) with
-      | PWildcard | PVariable _ | PTuple _ -> ()
-      | PTaggedValues (k, _) ->
-        let t', ktys = TypingEnvironment.lookup_tagged_union_type_from_tag tenv k in
-        if List.length ktys <> 1 then
-          error (Position.position pat) "This pattern is not irrefutable."
+    | PWildcard | PVariable _ | PTuple _ -> ()
+    | PTaggedValues (k, _) ->
+       let t', ktys = TypingEnvironment.lookup_tagged_union_type_from_tag tenv k in
+       if List.length ktys <> 1 then
+         error (Position.position pat) "This pattern is not irrefutable."
 
   and check_variable tenv ty x =
     TypingEnvironment.bind tenv x ty
@@ -386,31 +404,31 @@ let typecheck tenv ast =
     let ity = infer_expression_type tenv e in
     if ity <> xty then
       error (Position.position e) (
-        Printf.sprintf "Incompatible types.\nExpected: %s\nInferred: %s\n"
-          (HopixPrettyPrinter.(to_string typ xty))
-          (HopixPrettyPrinter.(to_string typ ity))
-      )
+              Printf.sprintf "Incompatible types.\nExpected: %s\nInferred: %s\n"
+			     (HopixPrettyPrinter.(to_string typ xty))
+			     (HopixPrettyPrinter.(to_string typ ity))
+	    )
 
   and check_same_length : type a b. Position.t -> a list -> b list -> unit = fun pos a b ->
-    let aln = List.length a and bln = List.length b in
-    if (aln <> bln) then (
-      error pos
-        (Printf.sprintf
-           "Invalid number of arguments.\nExpected: %d\nGiven: %d\n."
-           aln bln
-        )
-    )
+			    let aln = List.length a and bln = List.length b in
+			    if (aln <> bln) then (
+			      error pos
+				    (Printf.sprintf
+				       "Invalid number of arguments.\nExpected: %d\nGiven: %d\n."
+				       aln bln
+				    )
+			    )
 
-  and infer_literal_type = function
-    | LInt _ ->
-      tyint
+			   and infer_literal_type = function
+			     | LInt _ ->
+				tyint
 
-  and check_unicity
-      : type a b. Position.t -> (a * b) list -> string -> unit = fun pos ls what ->
-        let ls = List.(sort (fun (l1, _) (l2, _) -> compare l1 l2) ls) in
-        let ls = fst (List.split ls) in
-        if not (ExtStd.List.all_distinct ls) then
-          error pos (Printf.sprintf "Each %s must appear exactly once." what)
+			   and check_unicity
+			       : type a b. Position.t -> (a * b) list -> string -> unit = fun pos ls what ->
+				   let ls = List.(sort (fun (l1, _) (l2, _) -> compare l1 l2) ls) in
+				   let ls = fst (List.split ls) in
+				   if not (ExtStd.List.all_distinct ls) then
+				     error pos (Printf.sprintf "Each %s must appear exactly once." what)
 
-  in
-  program tenv ast
+				      in
+				      program tenv ast
