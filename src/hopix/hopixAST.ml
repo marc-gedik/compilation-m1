03a04c436f018d97509168dbbfc255291a09374b
GEDIK marc <gedik@informatique.univ-paris-diderot.fr>
2015-01-11 19:18:55 +0100

    Free Variables

--- src/hopix/hopixAST.ml
+++ src/hopix/hopixAST.ml
@@ -88,4 +88,55 @@ let is_binary_primitive = function
   | _ -> false
 
 let free_variables : expression -> identifier list =
-  fun e -> []
+  fun e ->
+  let getId id = fst (Position.value id) in
+  let fold f = List.fold_left (fun x set -> f set x) in
+  let remove_pattern set p =
+    match Position.value p with
+    | PWildcard -> set
+    | PVariable x -> VariableSet.remove x set
+    | PTuple xs
+    | PTaggedValues (_,xs) ->
+       fold VariableSet.remove set xs
+
+     in
+
+  let rec aux' e = aux (Position.value e)
+
+  and aux =
+    function
+    | Literal _ -> VariableSet.empty
+
+    | Variable (Id id as x) ->
+       if is_binary_primitive id then
+	 VariableSet.empty
+       else
+	 VariableSet.singleton x
+
+    | Define (p,e1,e2) -> remove_pattern (VariableSet.union (aux' e1) (aux' e2)) p (* without p *)
+    | Apply (e1,e2) -> VariableSet.union (aux' e1) (aux' e2)
+    | Fun ((x,_), e) -> VariableSet.remove x (aux' e) (* without x *)
+
+    | RecFuns fs ->
+       List.fold_left (fun set (id, e) ->
+		       VariableSet.add (getId id) (VariableSet.union (aux' e) set))
+		      VariableSet.empty fs
+
+    | Tuple es -> fold VariableSet.union VariableSet.empty (List.map aux' es)
+
+    | Record fs ->
+       List.fold_left (fun set (_, e) -> VariableSet.union (aux' e) set) VariableSet.empty fs
+
+    | RecordField (e, lbl) -> aux' e
+    | TaggedValues (k, es) -> VariableSet.empty
+    | IfThenElse (c, t, f) -> VariableSet.(union (aux' c) (union (aux' t)  (aux' f)))
+
+    | Case (e, bs) ->
+       List.fold_left (fun set (Branch(p,e)) ->
+		       remove_pattern (VariableSet.union set (aux' e)) p) (aux' e) bs
+
+    (* Only appears in the image of closure conversion. *)
+    | MutateTuple (e1, i, e2) -> failwith "Ast.free_variables: MutateTuple"
+
+  in VariableSet.elements (aux e)
+
